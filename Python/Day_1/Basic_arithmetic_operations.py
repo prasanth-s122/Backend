2a835b5c674162c9c640251186a13657324f9980
prasanth_s <prasanthsakthi122000@gmail.com>
2026-06-02 19:45:20 +0530

    first commit

---
+++ Python/Day_1/Basic_arithmetic_operations.py
@@ -0,0 +1,9 @@
+x = 100
+y = 6
+print("Number 1 is ",x)
+print("Number 2 is ",y)
+print("Sum is ",x+y)
+print("Subtraction is ",x-y)
+print("Division is ",x/y)
+print("Interior division is ",x//y)
+print("Modulus is ",x%y)

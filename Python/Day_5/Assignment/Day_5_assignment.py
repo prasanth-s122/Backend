05a60490f98de1f5858afa6d8f5642a0654ee61d
prasanth_s <prasanthsakthi122000@gmail.com>
2026-06-08 21:25:04 +0530

    Python day 5 assignment

---
+++ Python/Day_5/Assignment/Day_5_assignment.py
@@ -0,0 +1,14 @@
+g = {12,18,19,20,21,"hello"}
+print("The set is ---->",g)
+
+# add()
+g.add(100)
+print("After add() ---->",g)
+
+# update()
+g.update({22,25})
+print("After update() ---->",g)
+
+# remove()
+g.remove("hello")
+print("After remove() ---->",g)
